89f7a659b1531b7fe2479ade0e5a9dc59d143c95
JmLizarragaNodos <jlizarraga@nodos.cl>
2024-06-13 11:44:00 -0400

    Mostrar error carga pagina volver atras

--- TFL_x_WEB/MCTP_Disenio/configuraciones.js
+++ TFL_x_WEB/MCTP_Disenio/configuraciones.js
@@ -1,6 +1,6 @@
 //importarArchivoJS("../MCTP_Disenio/sesion.js");
 
-console.log("AppVersion, 2024.04.08 v1.1");
+console.log("AppVersion, 2024.06.13 v1.1");
 
 $("#appBody").adjustFontSize();   // Incrementar Tamaño de Fuente
 
